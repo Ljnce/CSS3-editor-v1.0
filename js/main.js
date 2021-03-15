ebf3da4a4a231885cab7a1da3810a9ea4daa2676
Luca <mylince@gmail.com>
2021-03-15 21:14:39 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+var container = document.createElement("div");
+container.id = 'style-editor';
+document.body.appendChild(container);
+
+// Selector
+var selector = document.createElement("textarea");
+selector.rows = '2';
+selector.cols = '35';
+selector.id = 'selector';
+selector.placeholder = 'Selector';
+selector.classList.add('welcome-animation');
+container.appendChild(selector);
+
+// Property
+var property = document.createElement("textarea");
+property.rows = '2';
+property.cols = '30';
+property.id = 'property';
+property.placeholder = 'Property';
+container.appendChild(property);
+
+// Css value
+var value = document.createElement("textarea");
+value.rows = '2';
+value.cols = '25';
+value.id = 'value';
+value.placeholder = 'Value + Press Enter';
+container.appendChild(value);
+
+// Add style with jQuery
+// $('#s').keypress(function(event){
+//     if (event.keyCode == 13) {
+//         var element = $('#element').val()
+//         var what = $('#what').val()
+//         var style = $('#style').val()
+//         $(el).css(what,style)
+//     }
+// });
+
+// Add style with JS
+var value = document.getElementById('value')
+value.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        var selector = document.getElementById('selector').value;
+        var property = document.getElementById('property').value;
+        var value = document.getElementById('value').value;
+        if (selector && selector.length && property && property.length && value && value.length) {
+            // $(selector).css(property,value);
+            var allSelector = document.querySelectorAll(selector);
+            for (i = 0; i < allSelector.length; i++) {
+                allSelector[i].style[property] = value;
+            }
+            console.log('Style on!');
+        }
+    }
+});
+
+// Add opacity on mouseover
+container.onmouseover = function(){
+    container.style.opacity = 1;
+};
+
+container.onmouseout = function(){
+    container.style.opacity = 0.3;
+};
+
+// Add welcome animation
+setTimeout(waitForWelcomeSecond, 100)
+
+function waitForWelcomeSecond(){
+    property.classList.add('welcome-animation');
+};
+
+setTimeout(waitForWelcomeThird, 200)
+function waitForWelcomeThird(){
+    value.classList.add('welcome-animation');
+};
